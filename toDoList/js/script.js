1ea2d2f25b3e49bca7db0ecddd8db23d7b216479
Lucas Prieto de Senna <lucas-prieto@hotmail.com>
2020-02-06 15:26:31 -0300

    update storage item

--- toDoList/js/script.js
+++ toDoList/js/script.js
@@ -2,13 +2,47 @@ let btnAddTarefa = document.querySelector('#adicionar-tarefa')
 let tarefas = document.querySelector('#tarefas')
 let inputTarefa = document.querySelector('#tarefa-digitada')
 let btnRmTarefa = document.querySelector('#botao-tarefa-ok')
+let listaTarefas = localStorage.getItem('listaTarefas')?JSON.parse(localStorage.getItem('listaTarefas')):[];
+
+const salvarLocalStorage = tarefas =>{
+    let tarefasEmJson = JSON.stringify(tarefas)
+    localStorage.setItem('listaTarefas', tarefasEmJson)
+    console.log('Lista de tarefas Salva!')
+}
+
+const mostrarTela = arrayTarefas=>{
+    arrayTarefas.forEach(textoTarefa => {
+        let tarefaNova = `<div class="col-md-4">
+        <div class="card-tarefa">
+            <div class="texto-tarefa">
+                ${textoTarefa}
+            </div>
+            <div id="botao-tarefa-ok">
+                <img src="./img/check.png" width="32" alt="Botão de Finalizar Tarefa"
+                    title="Botão de finalizar Tarefa">
+            </div>
+        </div>
+        </div>`
+
+        tarefas.insertAdjacentHTML('beforeend', tarefaNova)
+        let objTarefaNova = tarefas.lastElementChild
+        let btnCheckTarefa = objTarefaNova.lastElementChild.lastElementChild
+        btnCheckTarefa.onclick = (event) =>{
+            event.target.parentNode.parentNode.parentNode.remove()
+        }
+    })
+}
+
+mostrarTela(listaTarefas)
 
 const criarTarefa = (event) => {
     if (event.keyCode == 13 || event.type == "click") {
         let valorDigitado = inputTarefa.value;
-        if (valorDigitado == "") {
+        if(valorDigitado == "") {
             return (alert('Você deve digitar uma tarefa primeiro!'))
         }
+        listaTarefas.push(valorDigitado);
+        salvarLocalStorage(listaTarefas);
         inputTarefa.value = "";
         let tarefaNova = `<div class="col-md-4">
     <div class="card-tarefa">
@@ -19,6 +53,7 @@ const criarTarefa = (event) => {
             <img src="./img/check.png" width="32" alt="Botão de Finalizar Tarefa"
                 title="Botão de finalizar Tarefa">
         </div>
+    </div>
     </div>`
         tarefas.insertAdjacentHTML('beforeend', tarefaNova)
 
